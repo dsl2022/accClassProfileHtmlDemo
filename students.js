6fbc9cbdbe9fbcaa75abb36684b8f94f0b92b6fb
Marc Robinson <109494866+rbamarc@users.noreply.github.com>
2023-03-16 19:02:21 -0700

    added my info

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Marc',
+        lastName:'Robinson',
+        location: 'San Bernardino, CA',
+        html:'marcrobinson.html',
+        imagePath:'images/marcRobinson.jpg'
     },
 ]
 
