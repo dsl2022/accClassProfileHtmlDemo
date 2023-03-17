735a79f4f5fde1083a7d526f87b181428639786b
Amani Taylor <126356089+aisc07@users.noreply.github.com>
2023-03-16 19:09:07 -0700

    Update students.js

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Amani',
+        lastName:'Taylor',
+        location: 'California, CA',
+        html:'amaniTaylor.html',
+        imagePath:'images/amaniTaylor.jpg'
     },
 ]
 
