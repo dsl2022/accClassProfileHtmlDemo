79af0f0ba2417ad9ca1aaa7b2a1c8850be490c67
acwatt94 <126421564+acwatt94@users.noreply.github.com>
2023-03-16 19:03:15 -0700

    Update students.js
    Added personal info.

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Annaliesa',
+        lastName:'Watt',
+        location: 'Fullerton, ca',
+        html:'annaliesawatt.html',
+        imagePath:'images/annaliesaWatt.jpg'
     },
 ]
 
