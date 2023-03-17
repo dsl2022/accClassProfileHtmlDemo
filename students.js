e9e4eb1fbff1500f0b395b17759e75889fbc774a
aaronvanegas <126351267+aaronvanegas@users.noreply.github.com>
2023-03-16 19:02:18 -0700

    Update students.js
    I have added my info

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Aaron',
+        lastName:'Vanegas',
+        location: 'San Bernardino, CA',
+        html:'aaronVanegas.html',
+        imagePath:'images/aaronVanegas.jpg'
     },
 ]
 
