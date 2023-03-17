e7c80f2be0b394f0670eb06d336381a85093f755
lnunez002 <126374266+lnunez002@users.noreply.github.com>
2023-03-16 19:02:31 -0700

    add my info
    I have added infor

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Lily',
+        lastName:'Nunez',
+        location: 'Riverside, CA',
+        html:'lilynunez.html',
+        imagePath:'images/LilyNunez.jpg'
     },
 ]
 
