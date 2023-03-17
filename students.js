7581716443059ed306d00cd5f318b3ef7ecdf5ce
godracir <38054880+godracir@users.noreply.github.com>
2023-03-16 19:25:46 -0700

    Update students.js
    added my name

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Ricard',
+        lastName:'Garay',
+        location: 'Los Angeles, CA',
+        html:'ricardoGaray.html',
+        imagePath:'images/ricardoGaray.jpg'
     },
 ]
 
