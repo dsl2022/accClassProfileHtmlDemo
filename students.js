1a94d84f7a1f53a6bd5d6f51914b0e1e69be1c30
Sirvance <126606183+Sirvance@users.noreply.github.com>
2023-03-16 19:06:50 -0700

    Update students.js
    I add new branch

--- students.js
+++ students.js
@@ -2,16 +2,16 @@ export const data = [
     {
         firstName:'David',
         lastName:'Liang',
-        location: 'Philadephia, PA',
+        location: 'Philadephia,PA',
         html:'davidLiang.html',
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'vance',
+        lastName:'dowdell',
+        location: 'perris,ca',
+        html:'vancedowdell.html',
+        imagePath:'images/vancedowdell.jpg'
     },
 ]
 
