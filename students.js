10a7239695d7a42e2a6fcd78d41752557fd0a315
Tom <70374150+Thomas-JC@users.noreply.github.com>
2023-03-16 19:21:43 -0700

    Update students.js
    added my information

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Thomas',
+        lastName:'Cerda',
+        location: 'California, CA',
+        html:'thomasCerda.html',
+        imagePath:'images/thomasCerda.jpg'
     },
 ]
 
