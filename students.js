691136261caee58eaf5f6976790f1bb22549e6ae
tuankthai <ttlive4jc@gmail.com>
2023-03-16 19:04:00 -0700

    Update students.js

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Tuan',
+        lastName:'Thai',
+        location: 'Los Angeles, CA',
+        html:'tuanThai.html',
+        imagePath:'images/tuanThai.jpg'
     },
 ]
 
