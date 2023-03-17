1aba3e4df801066655e21ae1ce22a95837908413
Miguel F <126540792+OwlHD@users.noreply.github.com>
2023-03-16 19:02:27 -0700

    Update students.js
    added my info

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Miguel',
+        lastName:'Flores',
+        location: 'Garden Grove, CA',
+        html:'miguelFlores.html',
+        imagePath:'images/miguelFlores.jpg'
     },
 ]
 
