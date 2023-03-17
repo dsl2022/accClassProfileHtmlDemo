d6c2a8c4291b41c5eae69806816530129424ef17
Iva <84078385+Mistymos3@users.noreply.github.com>
2023-03-16 19:06:39 -0700

    Update students.js
    updated info

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Iva',
+        lastName:'Johnson',
+        location: 'California, CA',
+        html:'ivaJohnson.html',
+        imagePath:'images/ivaJohnson.jpg'
     },
 ]
 
