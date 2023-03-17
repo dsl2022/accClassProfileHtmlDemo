405f91068cf64976f2058bbe5d290ec8356fb574
davidso10 <126366725+davidso10@users.noreply.github.com>
2023-03-16 19:02:20 -0700

    added my info

--- students.js
+++ students.js
@@ -8,10 +8,10 @@ export const data = [
     },
     {
         firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        lastName:'Sanchez',
+        location: 'California, CA',
+        html:'davidsanchez.html',
+        imagePath:'images/davidSanchez.jpg'
     },
 ]
 
